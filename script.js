e9c96505bb0195f74b198c3746af8adc2ae30e61
Andrii Khlivnyi <akhlivnoy@gmail.com>
2023-03-14 19:42:34 +0200

    date input

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const START_DAY = new Date('2023-02-13T00:00:00');
-const currentDate = new Date();
+const dateInput = document.getElementById('date');
+const weekTypeElement = document.getElementById('week-type');
 
-const currentWeek =
-  Math.ceil(
-    (currentDate.getTime() - START_DAY.getTime()) / (7 * 24 * 60 * 60 * 1000)
-  ) & 1
-    ? 'Числитель'
-    : 'Знаменатель';
+function getWeekType(date) {
+  const currentWeek =
+    Math.ceil(
+      (date.getTime() - START_DAY.getTime()) / (7 * 24 * 60 * 60 * 1000)
+    ) & 1
+      ? 'Числитель'
+      : 'Знаменатель';
 
-document.getElementById('week-type').innerHTML = currentWeek;
+  return currentWeek;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const date = new Date();
+  const dateString = date.toJSON();
+  dateInput.value = dateString.slice(0, dateString.indexOf('T'));
+  weekTypeElement.innerHTML = getWeekType(date);
+});
+
+dateInput.addEventListener('change', e => {
+  weekTypeElement.innerHTML = getWeekType(new Date(e.target.value));
+});
